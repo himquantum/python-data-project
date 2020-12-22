a4bc6059b475279f770003bee86a3b73433ebec1
himquantum <51491877+himquantum@users.noreply.github.com>
2020-12-22 10:33:35 -0800

    Create connecting_flights.py

---
+++ connecting_flights.py
@@ -0,0 +1,30 @@
+# There is a list of itineraries that needs be sorted in such a way that it makes it easy for a user to go 
+# {[AT, NY], [LV, CH], [SF, LV], [CH, AT]}
+
+# I/P: {[AT, NY], [LV, CH], [SF, LV], [CH, AT]}
+# O/P: {[SF, LV], [LV, CH], [CH, AT], [AT, NY]}
+
+input_list = [['AT', 'NY'], ['LV', 'CH'], ['SF', 'LV'], ['CH', 'AT']]
+itinerary = []
+source = []
+destination = []
+output_list = []
+for flight in input_list:
+    source.append(flight[0])
+    destination.append(flight[1])
+
+for i in range(0, len(source)-1):
+    if source[i] not in destination:
+        start_flight = input_list[i]
+        break
+
+print(f'starting flight is {start_flight}')
+itinerary.append(start_flight)
+
+
+while len(input_list) > len(itinerary):
+    connecting_flight_source = itinerary[-1][1]
+    connecting_flight_index = source.index(connecting_flight_source)
+    next_flight = input_list[connecting_flight_index]
+    itinerary.append(next_flight)
+print(f'Dear passenger, your itinerary is {itinerary}')
